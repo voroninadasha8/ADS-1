ce4dd122f99265067855c2724c598f0d530a329c
voroninadasha8 <117026926+voroninadasha8@users.noreply.github.com>
2023-03-27 14:33:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,43 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  uint64_t i = 2;
+  for (i; i*i<=value; i++) {
+    if (value % i == 0)
+      return false;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  uint64_t res = 0;
+  uint64_t j = 0;
+  uint64_t k = 2;
+  for (k; j<n; k++) {
+    if (checkPrime(k)) {
+      res = i;
+      j++;
+    }
+    return res;
+  }
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  for (uint64_t m = value+1; ; i++) {
+    if (checkPrime(m)) {
+      return m;
+    }
+  }
 }
 
-uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+uint64_t sumPrime(uint64_t hbound) 
+  uint64_t count = 0;
+  uint64_t summa = 0;
+  int r = 0;
+  for (r; r , hbound; r++) {
+    if (checkPrime(r)) {
+      summa += r;
+    }
+  }
+  return summa-1;
 }
